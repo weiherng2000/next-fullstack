5d7eb8c409ccae0a196c18b172857cb332315487
Tan wei herng <weiherng2000@gmail.com>
2023-08-08 01:35:26 +0800

    photo

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -8,7 +8,7 @@ const  About = () => {
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image
-          src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src="pexels-photo.jpg"
           fill={true}
           alt=""
           className={styles.img}
