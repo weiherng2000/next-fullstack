7bb3e4dae92bf4eec7f4902edd02f99bb3f3dac5
Tan wei herng <weiherng2000@gmail.com>
2023-08-08 00:33:05 +0800

    ui

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
     <div className = "h-50 text-sm flex items-center justify-between">
       <div>©2023. All rights reserved.</div>
-      <div>
-        
+     
+      <div className='flex items-center gap-2'>
+           <Image width = {15} height = {15} src = "/1.png" alt='Test Dev' className='opacity-60 cursor-pointer'/>
+           <Image width = {15} height = {15} src = "/2.png" alt='Test Dev' className='opacity-60 cursor-pointer'/>
+           <Image width = {15} height = {15} src = "/3.png" alt='Test Dev' className='opacity-60 cursor-pointer'/>
+           <Image width = {15} height = {15} src = "/4.png" alt='Test Dev' className='opacity-60 cursor-pointer'/>
       </div>
+      
     </div>
   )
 }
