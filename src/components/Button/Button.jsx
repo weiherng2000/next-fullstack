1033056b731a74ff86295f0394c32d4c5ac45295
Tan wei herng <weiherng2000@gmail.com>
2023-08-08 00:49:30 +0800

    test

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../button.module.css";
+import styles from "../Button.module.css";
 import Link from "next/link";
 
 const Button = ({ text, url }) => {
