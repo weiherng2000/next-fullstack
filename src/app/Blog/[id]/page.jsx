a928939e6de6d31d6c727083d1b1f9fcc01c62ba
Tan wei herng <weiherng2000@gmail.com>
2023-08-11 21:39:05 +0800

    Update page.jsx

--- src/app/Blog/[id]/page.jsx
+++ src/app/Blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  const res = await fetch(`https://verdant-gecko-39f799.netlify.app//api/posts/${id}`, {
     cache: "no-store",
   });
 
